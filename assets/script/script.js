4963bb6c4691b493e0baf2a1ccc245d6bc06353f
RalphZam101 <rapoyrapoy101@gmail.com>
2023-04-24 17:52:24 +0800

    april 24 1v

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+// + - button
 function incrementRoom() {
   let value = parseInt(document.getElementById('rooms').value, 10);
   value = isNaN(value) ? 1 : value;
@@ -25,3 +26,26 @@ function decrementAdult() {
   value--;
   document.getElementById('adults').value = value < 1 ? 1 : value;
 }
+
+// calendar getting values and submit button
+const checkBtn = document.querySelector("#checkBtn");
+const todayDateInput = document.querySelector("#checkIn");
+const today = new Date();
+const todayDate = today.toISOString().substr(0, 10);
+
+todayDateInput.value = todayDate;
+
+checkBtn.addEventListener('click', checkAvail);
+
+function checkAvail(e) {
+  event.preventDefault();
+  const checkIn = document.getElementById('checkIn');
+  const checkinDate = checkIn.value;
+  console.log(checkinDate);
+
+  const checkOut = document.getElementById('checkOut');
+  const checkoutDate = checkOut.value;
+  console.log(checkoutDate);
+}
+
+
